c430dcd3622149554610831a2171cc156e345e4c
irfankazi <irfan.rv15@gmail.com>
2015-01-15 18:34:55 +0600

    Bulk Top Up Done

---
+++ Bulk Top Up/BulkInitRechargeSystem/BulkInitRechargeSystem/InitRechargeForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace BulkInitRechargeSystem
+{
+    partial class InitRechargeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 86);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(510, 19);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 127);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(421, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // InitRechargeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 306);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "InitRechargeForm";
+            this.Text = "InitRechargeSystem";
+            this.Load += new System.EventHandler(this.InitRechargeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Bulk Top Up/BulkInitRechargeSystem/BulkInitRechargeSystem/InitRechargeForm.cs
@@ -0,0 +1,229 @@
+﻿using BulkInitRechargeSystem.com.sslwireless.vrapi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BulkInitRechargeSystem
+{
+    public partial class InitRechargeForm : Form
+    {
+        virtualrechargeService proxy;
+        bulktopupEntities db;
+        string appflag;
+        public string appPath = "";
+        string location;
+        string Msg = "";
+        string total_exe;
+        string limit = "";
+        public InitRechargeForm()
+        {
+            InitializeComponent();
+            proxy = new virtualrechargeService();
+        }
+
+        private void InitRechargeForm_Load(object sender, EventArgs e)
+        {
+
+            GetFileInfo();
+
+            checkedListBox1.Visible = false;
+            listBox1.Visible = false;
+
+
+            timer1.Start();
+            Msg = "Timer1 is Started";
+            label1.Text = Msg;
+            this.Text = "InitRechargeApp" + appflag;
+        }
+
+                  
+        private void GetFileInfo()
+        {
+            try
+            {
+                location = System.IO.Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath);
+                FileStream file = new FileStream(location + "\\info.txt", FileMode.OpenOrCreate, FileAccess.Read);
+
+                StreamReader sr = new StreamReader(file);
+                total_exe = sr.ReadLine();
+                appflag = sr.ReadLine();
+                limit = sr.ReadLine();
+                sr.Close();
+                file.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+
+
+        private void CreateLog(String msg)
+        {
+            FileStream fp = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "//Log.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fp);
+            sw.WriteLine(DateTime.Now.ToString() + " ==> " + msg);
+            sw.Close();
+            fp.Close();
+        }
+        private void CreateErrorLog(String msg)
+        {
+            FileStream fp = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "//ErrorLog/Log.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fp);
+            sw.WriteLine(DateTime.Now.ToString() + " ==> " + msg);
+            sw.Close();
+            fp.Close();
+        }
+
+        
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            
+            timer1.Stop();
+            CreateLog("Timer1 Stopped");
+            Msg = "Timer1 is Stopped";
+            label1.Text = Msg;
+            
+                int TryToConnect = 0;
+                while (TryToConnect < 5)
+                {
+                    try
+                    {
+                        db = new bulktopupEntities();
+
+                        var ProcessingData = db.RechargeInfoTbls.AsEnumerable().Where(g => g.create_recharge_Status == "Success" && g.vr_guid != null && g.init_recharge_status != "Success" && g.Init_try < 1 && g.id % Convert.ToInt32(total_exe) == Convert.ToInt32(appflag)).OrderByDescending(f => f.id).Take(Convert.ToInt32(limit)).ToList();
+
+                            if (ProcessingData.Count() != 0)
+                            {
+                                listBox1.Visible = true;
+                                Msg = ProcessingData.Count().ToString() + " Data Found to be processed  ";
+                                CreateLog(Msg);
+                                label1.Text = Msg;
+                             
+                                foreach (var i in ProcessingData)
+                                {
+                                       var InitRecharge = proxy.InitRecharge(i.client_id, i.client_pass, i.guid, i.vr_guid, "http://192.168.69.210/VR_Success/", "00");
+                                        var FindOrginal = db.RechargeInfoTbls.First(g => g.id == i.id);
+
+                                        if (InitRecharge.recharge_status == 200)
+                                        {
+                                            FindOrginal.init_recharge_status = "Success";
+                                            FindOrginal.Init_Status_Code = InitRecharge.recharge_status.ToString();
+                                            FindOrginal.Init_try = FindOrginal.Init_try + 100;
+                                            FindOrginal.ResponseDateTime = DateTime.Now;
+                                            db.SaveChanges();
+                                            CreateLog("Recharge Request id: " + i.id + " Mobile No: " + i.recepient_msisdn + " Processed");
+                                            listBox1.Items.Add("Recharge Request id: " + i.id + " Mobile No: " + i.recepient_msisdn + " Processed");
+                                        }
+
+
+                                        else
+                                        {
+                                            FindOrginal.init_recharge_status = "Error: " + InitRecharge.message;
+                                            FindOrginal.Init_Status_Code = InitRecharge.recharge_status.ToString(); 
+                                            FindOrginal.Init_try = FindOrginal.Init_try + 1;
+                                            FindOrginal.ResponseDateTime = DateTime.Now;
+                                            db.SaveChanges();
+                                            listBox1.Items.Add("Recharge Request id: " + i.id + " Can't Be recharged" + InitRecharge.message);
+                                            // listBox1.ForeColor = Color.Red;                                           
+                                          CreateErrorLog("Recharge Request id: " + i.id + " Can't Be recharged" + InitRecharge.message);
+
+                                        }
+                                  
+                                
+                                }
+                            }
+                            else
+                            {
+                                CreateLog("No data Found to be processed");
+                                Msg = "No data Found to be processed So far";
+                                label1.Text = Msg;                                                            
+                            }
+                      
+                        TryToConnect = 0;
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.InnerException != null)
+                        {
+                            Msg = ex.InnerException.ToString();
+                            label1.Text = Msg;
+                            CreateErrorLog(ex.Message + ex.InnerException + ex.Source);
+                        }
+                        else
+                        {                           
+                            Msg = ex.Message;
+                            label1.Text = Msg;
+                            CreateErrorLog(ex.Message);
+                        }
+                        if (db.Database.Connection.State != ConnectionState.Open)
+                        {
+                            CreateErrorLog("Database Connection is not Open and Tried  for " + TryToConnect + " Times");
+                            TryToConnect++;
+                            Thread.Sleep(2000); 
+
+                        }
+                        else
+                        {
+                            TryToConnect = 0;
+                        }
+                    }
+
+                    if (TryToConnect == 0)
+                        break;
+                }
+
+
+            timer1.Interval = 5000;
+            timer1.Start();
+
+
+
+        }
+
+        public bool CheckServiceAccess(string ServiceUrl)
+        {
+
+            bool Response;
+            try
+            {
+                var myRequest = (HttpWebRequest)WebRequest.Create(ServiceUrl);
+
+                var response = (HttpWebResponse)myRequest.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    //  it's at least in some way responsive
+                    //  but may be internally broken
+                    //  as you could find out if you called one of the methods for real
+                    // Response = string.Format("{0} is  Available", ServiceUrl);
+                    Response = true;
+
+                }
+                else
+                {
+                    Response = false;
+                    //  well, at least it returned...
+                    // Response = string.Format("{0} Returned, but with status: {1}", ServiceUrl, response.StatusDescription);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //  not available at all, for some reason
+                // Response = string.Format("{0}  is unavailable: {1}", ServiceUrl, ex.Message);
+                Response = false;
+            }
+            return Response;
+        }
+    }
+}
